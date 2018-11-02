09bb13ec8a61f0e90d8bf3cfd6ffb491d5271e63
Polina Bochkareva <polinabochk@gmail.com>
2018-11-01 21:15:25 -0700

    Add packages for Module 1,2,3

---
+++ src/main/java/com/Module1_JavaLanguage/task4/Language.java
@@ -0,0 +1,13 @@
+package main.java.com.Module1_JavaLanguage.task4;
+/**
+ *  Language enum class for Task #4.
+ *
+ *  This class represents types of language classes that are used in LanguageGroups class
+ *
+ *  @version 1.0 10/16/2018
+ *  @author Polina Bochkareva
+ */
+
+public enum Language {
+    ENGLISH, SPANISH
+}
