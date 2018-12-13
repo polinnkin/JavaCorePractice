d1354c9fa138aead0461a5eae9cf009ec6aa3b19
Polina <polinabochk@gmail.com>
2018-12-12 18:37:06 -0800

    Merge branch 'master' of https://github.com/polinnkin/JavaCorePractice
    Add test folder

---
+++ src/test/java/Module1_JavaLanguage/task1/TriangleTest.java
@@ -0,0 +1,22 @@
+package test.java.Module1_JavaLanguage.task1;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class TriangleTest {
+
+    @BeforeMethod
+    public void setUp() {
+    }
+
+    @AfterMethod
+    public void tearDown() {
+    }
+
+    @Test
+    public void testMain() {
+    }
+}
